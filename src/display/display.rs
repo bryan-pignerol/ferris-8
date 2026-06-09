632b0925f7b3d89cb69dcbd929fe0aa655572ec6
bryan-pignerol <br.pignerol@gmail.com>
2026-06-09 15:20:53 +0200

    First commit

---
+++ src/display/display.rs
@@ -0,0 +1,63 @@
+use minifb::{Key, Window, WindowOptions, Scale};
+
+/// The struct used to create the application window.
+pub struct Video {
+    window: Window,
+    buffer: Vec<u32>,
+    width: usize,
+    height: usize,
+}
+
+impl Video {
+    // VIDEO
+    /// Create the window.
+    pub fn new(width: usize, height: usize) -> Self {
+        let mut buffer: Vec<u32> = vec![0; width * height];
+
+        let mut options: WindowOptions = WindowOptions::default();
+        options.scale = Scale::X4;
+
+        let mut window = Window::new(
+            "My Fantasy Console in Rust !!!",
+            width,
+            height,
+            options,
+        )
+        .unwrap_or_else(|e| {
+            panic!("{}", e)
+        });
+
+        window.set_target_fps(60);
+
+        Self { window, buffer, width, height }
+    }
+
+    /// Clear the window.
+    pub fn clear(&mut self, color: u32) {
+        for pixel in self.buffer.iter_mut() {
+            *pixel = color;
+        }
+    }
+
+    /// Update the window.
+    pub fn update(&mut self) -> bool {
+        self.window
+            .update_with_buffer(&self.buffer, self.width, self.height)
+            .unwrap();
+
+        self.window.is_open() && !self.window.is_key_down(Key::Escape)
+    }
+
+    // DRAWINGS
+    pub fn draw_pixel(&mut self, x: usize, y: usize, color: u32) {
+        if x < self.width && y < self.height {
+            let index = y * self.width + x;
+
+            self.buffer[index] = color;
+        }
+    }
+
+    pub fn draw_line(&mut self, x1: usize, y1: usize, x2: usize, y2: usize, color: u32) {}
+
+    pub fn draw_rect(&mut self, x1: usize, y1: usize, x2: usize, y2: usize, color: u32) {}
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod display;
+use display::Video;
+
+
+const WINDOW_WIDTH: usize = 128;
+const WINDOW_HEIGHT: usize = 128;
+
+
+fn main() {
+    println!("Fantasy Console is Started !");
+
+    let mut video: Video = display::Video::new(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    let black_color = 0x000000;
+
+    while video.update() {
+        video.clear(black_color);
+
+        // GET KEYBOARD INPUTS
+
+        // EXECUTE LUA SCRIPT
+
+        // DRAW
+        let red_color = 0xFF0000;
+        video.draw_pixel(64, 64, red_color);
+    }
+
+    println!("Fantasy Console is closed !");
+}
